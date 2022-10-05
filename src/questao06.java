443c51e437b08d768c9ca912bc66d1d11a1fbdaf
Felipe Amador <felipedavidamador@gmail.com>
2022-10-05 13:32:30 -0300

    feat: add question six answer

---
+++ src/questao06.java
@@ -0,0 +1,8 @@
+public class questao06 {
+    public static void main(String[] args) {
+        System.out.println("Felipe Amador");
+        System.out.println("Pain");
+
+        // TODO: @mikael, coloca o teu aqui
+    }
+}
